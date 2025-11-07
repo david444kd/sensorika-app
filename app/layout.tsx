d83640984ace62ccd6cd94b5e3eeb8953d6a4704
david444kd <dkozahmetov132@gmail.com>
2025-11-07 13:11:05 +0500

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,43 +1,42 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header"
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Header from '@/components/Header'
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "AI Chat Assistant",
-  description: "Experience intelligent conversations powered by advanced AI",
+  title: 'Sensorika app',
+  description:
+    'Инструменты, советы для развития вашего ребенка — в одном месте. Понятно. Удобно. С любовью.',
   viewport: {
-    width: "device-width",
+    width: 'device-width',
     initialScale: 1,
     maximumScale: 5,
   },
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
-        <div className="max-w-2xl mx-auto">
-          {children}
-        </div>
+        <div className='max-w-2xl mx-auto'>{children}</div>
       </body>
     </html>
-  );
+  )
 }
