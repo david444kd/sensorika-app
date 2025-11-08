6bfee29551b50c31183f1a23efcd3641d2ce6a24
david444kd <dkozahmetov132@gmail.com>
2025-11-08 20:08:14 +0500

    fix img

--- app/[locale]/cards/page.tsx
+++ app/[locale]/cards/page.tsx
@@ -1,24 +1,21 @@
-"use client"
+'use client'
 
-import { getCards, Category } from "@/lib/cards/cards"
-import { Toaster } from "react-hot-toast"
-import PecsCard from "@/components/cards/PecsCard"
-import { useState, useMemo } from "react"
-import ToggleCategory from "@/components/cards/ToggleCategory"
-import { useTranslations } from "next-intl"
+import { Category, CARDS } from '@/lib/cards/cards'
+import { Toaster } from 'react-hot-toast'
+import PecsCard from '@/components/cards/PecsCard'
+import { useState } from 'react'
+import ToggleCategory from '@/components/cards/ToggleCategory'
 
 export default function CardsPage() {
-  const t = useTranslations()
-  const [category, setCategory] = useState<Category>("eat")
-  const CARDS = useMemo(() => getCards(t), [t])
+  const [category, setCategory] = useState<Category>('eat')
   const items = CARDS[category]
 
   return (
-    <main className="flex flex-col gap-6 p-6">
-     <ToggleCategory category={category} setCategory={setCategory}/>
-      <section className="grid grid-cols-2  gap-4">
+    <main className='flex flex-col gap-6 p-6'>
+      <ToggleCategory category={category} setCategory={setCategory} />
+      <section className='grid grid-cols-2  gap-4'>
         {items.map((it, idx) => (
-          <PecsCard item={it} key={idx}/>
+          <PecsCard item={it} key={idx} />
         ))}
       </section>
       <Toaster
