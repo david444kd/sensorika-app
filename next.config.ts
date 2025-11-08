a5f9e4d39940fc29d7930e532215e93aafc47684
david444kd <dkozahmetov132@gmail.com>
2025-11-08 19:42:16 +0500

    fix images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,26 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:locale/eat/:path*',
+        destination: '/eat/:path*', 
+      },
+      {
+        source: '/:locale/games/:path*',
+        destination: '/games/:path*',
+      },
+      {
+        source: '/:locale/actions/:path*',
+        destination: '/actions/:path*',
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig);
